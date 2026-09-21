0e3fd780745b6a4bc4c9481e90375ec242ca3db8
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-21 22:41:41 +0530

    Delete core/runtime/test_metrics.py

--- core/runtime/test_metrics.py
+++
@@ -1,4 +0,0 @@
-from core.runtime.metrics import RuntimeMetrics
-
-def test_metrics_completion_rate():
-    m=RuntimeMetrics(); m.record_start(); m.record_complete(); assert m.completion_rate == 1.0
