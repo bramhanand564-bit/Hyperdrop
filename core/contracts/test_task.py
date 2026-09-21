8d98e9ff2b8a56cf142913c14fa6f826bf0e503e
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-21 22:15:16 +0530

    Delete core/contracts/test_task.py

--- core/contracts/test_task.py
+++
@@ -1,7 +0,0 @@
-from task import Task, TaskStatus
-
-
-def test_task_starts_pending():
-    task = Task(id="t1", goal="find meaning")
-    assert task.status == TaskStatus.PENDING
-    assert task.knowledge_state == "unknown"
