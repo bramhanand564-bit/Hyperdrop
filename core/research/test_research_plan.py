da62a247a08c32532e6cb24565175244b6d95b08
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-21 22:40:31 +0530

    Delete core/research/test_research_plan.py

--- core/research/test_research_plan.py
+++
@@ -1,7 +0,0 @@
-from research_plan import build_plan
-
-
-def test_research_plan_contains_goal_query():
-    plan = build_plan("t1", "meaning of a word")
-    assert plan.task_id == "t1"
-    assert plan.steps[0].query == "meaning of a word"
