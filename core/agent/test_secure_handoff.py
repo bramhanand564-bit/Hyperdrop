feeb41d75de53e12e4559ce0e45e37d60dded608
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-21 22:12:46 +0530

    Delete core/agent/test_secure_handoff.py

--- core/agent/test_secure_handoff.py
+++
@@ -1,4 +0,0 @@
-from core.agent.secure_handoff import HandoffReason, requires_human_handoff
-def test_sensitive_boundaries_require_handoff():
-    assert requires_human_handoff(HandoffReason.OTP)
-    assert requires_human_handoff(HandoffReason.SENSITIVE)
