2f3ed833026d8542436c0bcdcb58c5dcab697710
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-21 22:14:11 +0530

    Delete core/cognition/test_benchmark_compare.py

--- core/cognition/test_benchmark_compare.py
+++
@@ -1,5 +0,0 @@
-from core.cognition.benchmark_compare import compare
-
-def test_compare_calculates_deltas():
-    result=compare(.40,.60,.45,.70)
-    assert result.delta_pass_rate==.20 and result.delta_average_score==.25
