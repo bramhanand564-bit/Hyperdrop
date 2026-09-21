b132d5ffe36621dfce4dbd62704fe405aeb35fec
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-21 22:09:51 +0530

    Delete android/app/src/main/java/com/hyperdrop/AndroidRuntimeController.kt

--- android/app/src/main/java/com/hyperdrop/AndroidRuntimeController.kt
+++
@@ -1,6 +0,0 @@
-package com.hyperdrop
-
-class AndroidRuntimeController(private val serviceIntentName: String = "com.hyperdrop.HyperdropForegroundService") {
-    fun runtimeServiceName(): String = serviceIntentName
-    fun killRuntime(): String = "kill_runtime"
-}
