aaba0a641a25e4258d8a872fbe714b6aff0276a1
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-21 22:16:23 +0530

    Delete core/memory/test_relevance.py

--- core/memory/test_relevance.py
+++
@@ -1,8 +0,0 @@
-from core.memory.lesson import Lesson
-from core.memory.relevance import memory_score, should_forget
-
-
-def test_relevance_and_forgetting():
-    item = Lesson("Python", "language", .9)
-    assert memory_score(item, "Python") > .6
-    assert should_forget(Lesson("x", "weak", .2)) is True
