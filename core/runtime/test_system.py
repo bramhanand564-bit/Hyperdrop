cab8033507d467f81391c8bb5d6cfe2a0ff6be8f
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-21 22:41:53 +0530

    Delete core/runtime/test_system.py

--- core/runtime/test_system.py
+++
@@ -1,8 +0,0 @@
-from core.runtime.system import HyperdropRuntime
-
-
-def test_runtime_composes_services(tmp_path):
-    runtime = HyperdropRuntime(runtime_config := None)
-    assert runtime.memory is not None
-    assert runtime.tasks is not None
-    assert runtime.permissions.allowed("browser") is False
