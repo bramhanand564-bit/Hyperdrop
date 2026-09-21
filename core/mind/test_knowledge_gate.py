84d9924cd8e78bd0119a503839fd42c95969b0e8
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-21 22:17:22 +0530

    Delete core/mind/test_knowledge_gate.py

--- core/mind/test_knowledge_gate.py
+++
@@ -1,9 +0,0 @@
-from core.mind.knowledge_gate import classify_knowledge
-from core.mind.knowledge_state import KnowledgeState
-
-
-def test_knowledge_gate():
-    assert classify_knowledge(known=True, confidence=.9) == KnowledgeState.KNOWN
-    assert classify_knowledge(known=True, confidence=.6) == KnowledgeState.UNCERTAIN
-    assert classify_knowledge(known=False, confidence=0) == KnowledgeState.UNKNOWN
-    assert classify_knowledge(known=True, confidence=.9, conflicting=True) == KnowledgeState.CONFLICTING
