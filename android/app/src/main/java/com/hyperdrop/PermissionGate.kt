90eb7b3638fd3365cd14e0938ddc6e579e878c08
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-21 22:10:07 +0530

    Delete android/app/src/main/java/com/hyperdrop/PermissionGate.kt

--- android/app/src/main/java/com/hyperdrop/PermissionGate.kt
+++
@@ -1,6 +0,0 @@
-package com.hyperdrop
-
-class PermissionGate {
-    fun requiresConfirmation(tool: String): Boolean =
-        tool in setOf("browser_login","upload","download","send_message","purchase","delete")
-}
