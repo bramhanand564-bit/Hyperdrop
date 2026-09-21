b70783ec036bb781f8474a501bc843b285d3d8da
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-21 22:14:26 +0530

    Delete core/cognition/test_dataset_guard.py

--- core/cognition/test_dataset_guard.py
+++
@@ -1,9 +0,0 @@
-from core.cognition.dataset_guard import validate_train_eval
-
-def test_guard_rejects_exact_leakage():
-    ok,reasons=validate_train_eval([{"instruction":"same"}],[{"instruction":"same"}])
-    assert not ok and "train_eval_exact_overlap" in reasons
-
-def test_guard_accepts_clean_split():
-    ok,reasons=validate_train_eval([{"instruction":"train"}],[{"instruction":"eval"}])
-    assert ok and not reasons
