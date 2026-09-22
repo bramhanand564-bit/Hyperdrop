2ff1826c87dbc5553650d81e0b4e81d4eae53a6c
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-22 21:29:47 +0530

    Support wildcard Event Bus subscribers

--- event-bus/EventBus.js
+++ event-bus/EventBus.js
@@ -5,7 +5,7 @@ const emitter = new EventEmitter();
 export const EventBus = {
   on: (type, listener) => emitter.on(type, listener),
   off: (type, listener) => emitter.off(type, listener),
-  emit: (type, payload={}) => emitter.emit(type, { ...payload, type, timestamp: Date.now() }),
+  emit: (type, payload={}) => emitter.emit(type, { ...payload, type, timestamp: Date.now() }); emitter.emit('*', { ...payload, type, timestamp: Date.now() }),
   clear: (type) => emitter.clear(type),
 };
 
