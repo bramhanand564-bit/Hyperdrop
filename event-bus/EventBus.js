12ec19df2ad7e98750761340597612ee67c489a6
bramhanand564-bit <bramhanand564@gmail.com>
2026-09-22 21:24:14 +0530

    Phase 8: add Nax Event Bus foundation

---
+++ event-bus/EventBus.js
@@ -0,0 +1,12 @@
+import EventEmitter from './EventEmitter';
+
+const emitter = new EventEmitter();
+
+export const EventBus = {
+  on: (type, listener) => emitter.on(type, listener),
+  off: (type, listener) => emitter.off(type, listener),
+  emit: (type, payload={}) => emitter.emit(type, { ...payload, type, timestamp: Date.now() }),
+  clear: (type) => emitter.clear(type),
+};
+
+export default EventBus;
